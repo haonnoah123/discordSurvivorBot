d5bcd1f18411b05dd3362491ad3ac8d33e50fdcd
Noah Rubin <95647477+haonnoah123@users.noreply.github.com>
2022-01-14 11:17:02 -0600

    Update Players.java
    Added: has voted, hasPlayedIdol, and isIn, also added User to identify players

--- Players.java
+++ Players.java
@@ -1,22 +1,55 @@
 package survivor;
 
-public class Players implements java.io.Serializable{
+import net.dv8tion.jda.api.entities.User;
+
+public class Players{
 	
 	private String name;
+	private User user;
 	private int idolCount;
 	private int votesAgainst;
+	private boolean isIn;
+	private boolean hasVoted;
+	private boolean hasPlayedIdol;
 	
-	public Players(String names) {
+	public Players(String names, User u) {
 		name = names;
+		user = u;
 		idolCount = 0;
+		isIn = true;
+		hasVoted = false;
+		hasPlayedIdol = false;
+	}
+	
+	public void newDay() {
+		votesAgainst = 0;
+		hasVoted = false;
+		hasPlayedIdol = false;
+	}
+	
+	public void playIdol() {
+		hasPlayedIdol = true;
+		idolCount--;
+	}
+	
+	public void unPlayIdol() {
+		hasPlayedIdol = false;
+	}
+	
+	public void didVote() {
+		hasVoted = true;
+	}
+	
+	public void newTribal() {
+		hasVoted = false;
+	}
+	
+	public void voteOut() {
+		isIn = false;
 	}
 	
 	public void addIdol() {
 		idolCount++;
-	}
-	
-	public void useIdol() {
-		idolCount--;
 	}
 	
 	public void addVote() {
@@ -41,5 +74,21 @@ public class Players implements java.io.Serializable{
 	
 	public int getVotesAgainst() {
 		return votesAgainst;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public boolean getHasVoted() {
+		return hasVoted;
+	}
+	
+	public boolean getIsIn() {
+		return isIn;
+	}
+	
+	public boolean getHasPlayedIdol() {
+		return hasPlayedIdol;
 	}
 }
